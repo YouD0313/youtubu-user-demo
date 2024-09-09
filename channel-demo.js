0876bea215803a1bca3ae0568e89fcd0896263e1
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2024-09-09 18:04:35 +0900

    채널생성, 채널개별수정, 채널개별삭제, 채널전체조회, 채널개별조회  구현

---
+++ channel-demo.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const app = express();
+app.listen(7777);
+app.use(express.json());
+
+const users = [];
+
+// obj length check
+const isExist = (obj) => {
+	const objLength = Object.keys(obj).length;
+	if (objLength > 0) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+// userCheck
+const idCheck = (obj, id, channelTitle) => {
+	let userSearch = {};
+	obj.map((user, idx) => {
+		channelTitle && user.channelTitle === channelTitle ? (userSearch = 3) : '';
+		if (user.channelTitle === id) {
+			return (userSearch = { idx, ...user });
+		}
+	});
+	return userSearch;
+};
+
+app
+	.route('/channels')
+	// 채널생성
+	.post((req, res) => {
+		const { channelTitle } = req.body;
+		const key = users.filter((user) => user.channelTitle === channelTitle);
+		if (isExist(req.body)) {
+			if (key.length > 0) {
+				res.status(409).json({
+					message: `${channelTitle}은 이미 있는 채널명입니다.`,
+				});
+			} else {
+				users.push({ channelTitle });
+				res.status(201).json({
+					message: `${channelTitle}님 채널을 응원합니다.`,
+				});
+			}
+		} else {
+			res.status(400).json({
+				message: `채널명을 올바르게 입력하세요.`,
+			});
+		}
+	})
+	// 채널전체조회
+	.get((req, res) => {
+		let usersCheck = {};
+		users.map((user, idx) => {
+			usersCheck[idx] = { ...user };
+		});
+		const usersLength = Object.keys(usersCheck).length;
+		if (usersLength > 0) {
+			res.status(200).json(usersCheck);
+		} else {
+			res.status(500).json({
+				message: `채널이 하나도 존재하지 않습니다.`,
+			});
+		}
+	});
+
+app
+	.route('/channels/:id')
+	// 채널개별수정
+	.put((req, res) => {
+		const { id } = req.params;
+		const { channelTitle } = req.body;
+		const userCheck = idCheck(users, id, channelTitle);
+		const userCheckLength = Object.keys(userCheck).length;
+
+		if (userCheckLength > 0) {
+			users[userCheck.idx].channelTitle = channelTitle;
+			res.status(200).json({
+				message: `${id}님의 채널명이 ${channelTitle}로 성공적으로 수정되었습니다.`,
+			});
+		} else if (userCheck == 3) {
+			res.status(404).json({
+				message: `${channelTitle}는 이미 있는 채널명입니다.`,
+			});
+		} else {
+			res.status(404).json({
+				message: `${id}는 없는 채널명입니다.`,
+			});
+		}
+	})
+	// 채널개별삭제
+	.delete((req, res) => {
+		const { id } = req.params;
+		const userCheck = idCheck(users, id);
+		const userCheckLength = Object.keys(userCheck).length;
+
+		if (userCheckLength > 0) {
+			users.splice(userCheck.idx, 1);
+			res.status(200).json({
+				message: `${id}님의 채널이 삭제되었습니다.`,
+			});
+		} else {
+			res.status(404).json({
+				message: `${id}님의 채널이 존재하지않습니다.`,
+			});
+		}
+	})
+	// 채널개별조회
+	.get((req, res) => {
+		const { id } = req.params;
+		const userCheck = idCheck(users, id);
+		const userCheckLength = Object.keys(userCheck).length;
+
+		if (userCheckLength > 0) {
+			res.status(200).json(userCheck);
+		} else {
+			res.status(400).json({
+				message: `${id}는 존재하지 않는 채널입니다.`,
+			});
+		}
+	});
--- user-demo.js
+++ user-demo.js
@@ -6,77 +6,104 @@ app.use(express.json());
 
 const users = [];
 
+// idCheck
+const idCheck = (obj, id) => {
+	let loginUser = {};
+	obj.map((user, idx) => {
+		if (user.id === id) {
+			loginUser = { ...user, idx };
+		}
+	});
+	return loginUser;
+};
+
+// pwdCheck
+const pwdCheck = (obj, pwd, res) => {
+	if (obj.pwd === pwd) {
+		res.status(200).json({
+			message: `${obj.id}님 환영합니다.`,
+		});
+	} else {
+		res.status(400).json({
+			message: `pw아이디나 비밀번호를 다시 확인해주세요.`,
+		});
+	}
+};
+
 // 로그인
 app.post('/login', (req, res) => {
 	const { id, pwd } = req.body;
-	users.map((user) => {
-		if (user.id === id) {
-			if (user.pwd === pwd) {
-				res.json({
-					message: `${id}님 환영합니다.`,
-				});
-			} else {
-				res.status(400).json({
-					message: `아이디나 비밀번호를 다시 확인해주세요.`,
-				});
-			}
-		} else {
-			res.status(400).json({
-				message: `아이디나 비밀번호를 다시 확인해주세요.`,
-			});
-		}
-	});
+	const loginUser = idCheck(users, id);
+	const userCheck = Object.keys(loginUser);
+
+	if (userCheck.length > 0) {
+		pwdCheck(loginUser, pwd, res);
+	} else {
+		res.status(400).json({
+			message: `id아이디나 비밀번호를 다시 확인해주세요.`,
+		});
+	}
 });
 
 // 회원가입
 app.post('/join', (req, res) => {
 	const { id, pwd, name } = req.body;
 	const key = users.filter((user) => user.id === id);
-	if (key.length === 0) {
-		users.push({ id, pwd, name });
-		res.json({
-			message: `${id}님 가입을 환영합니다.`,
-		});
+	const bodyKeyCheck = Object.keys(req.body).length;
+	if (bodyKeyCheck > 0) {
+		if (key.length === 0) {
+			users.push({ id, pwd, name });
+			res.status(201).json({
+				message: `${id}님 가입을 환영합니다.`,
+			});
+		} else {
+			res.status(409).json({
+				message: `이미 존재하는 아이디입니다.`,
+			});
+		}
 	} else {
-		res.json({
-			message: `이미 존재하는 아이디입니다.`,
+		res.status(400).json({
+			message: `입력 값을 다시 확인해주세요.`,
 		});
 	}
 });
 
-// 회원개별조회
-app.get('/users/:id', (req, res) => {
-	const { id } = req.params;
-	if (users.length === 0) {
-		res.json({
-			message: `존재하지 않는 계정입니다.`,
-		});
-		return;
-	}
-	users.map((user) => {
-		if (user.id === id) {
-			res.json({ id: user.id, name: user.name });
+// route 맛보기
+app
+	.route('/users/:id')
+	// 회원개별조회
+	.get((req, res) => {
+		const { id } = req.params;
+		if (users.length === 0) {
+			res.status(500).json({
+				message: `회원이 존재하지 않습니다.`,
+			});
+			return;
+		}
+		const loginUser = idCheck(users, id);
+		const userCheck = Object.keys(loginUser);
+		if (userCheck.length > 0) {
+			res.status(200).json({ id: loginUser.id, name: loginUser.name });
 		} else {
-			res.json({
+			res.status(404).json({
+				message: `존재하지 않는 계정입니다.`,
+			});
+		}
+	})
+	// 회원개별탈퇴
+	.delete((req, res) => {
+		const { id } = req.params;
+		const loginUser = idCheck(users, id);
+		const userCheck = Object.keys(loginUser);
+
+		if (userCheck.length > 0) {
+			users.splice(loginUser.idx, 1);
+			res.status(200).json({
+				message: `${id}님의 계정이 탈퇴되었습니다.`,
+			});
+		} else {
+			res.status(404).json({
 				message: `존재하지 않는 계정입니다.`,
 			});
 		}
 	});
-});
-
-// 회원개별탈퇴
-app.delete('/users/:id', (req, res) => {
-	const { id } = req.params;
-	users.map((user, idx) => {
-		if (user.id === id) {
-			users.splice(idx, 1);
-			res.json({
-				message: `${id}님의 계정이 탈퇴되었습니다.`,
-			});
-		}
-		console.log(users);
-	});
-	res.json({
-		message: `존재하지 않는 계정입니다.`,
-	});
-});
