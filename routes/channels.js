29efe99856fa346ce9bab0bfb058c1f287c5e14d
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2024-09-25 18:26:29 +0900

    미들웨어설정, express-validatior 사용, jwt .env 쿠키에 넣어 response 코드 구현

--- routes/channels.js
+++ routes/channels.js
@@ -1,129 +1,232 @@
 import { Router } from 'express';
 const router = Router();
 import conn from '../mariadb.js';
+import { body, param, query, validationResult } from 'express-validator';
+
+const validation = (req, res, next) => {
+	const err = validationResult(req);
+	if (err.isEmpty()) {
+		return next();
+	} else {
+		const msg = err
+			.array()
+			.filter((n) => n.msg !== 'Invalid value')
+			.map((n) => n.msg)
+			.join('');
+
+		return res
+			.status(404)
+			.json({ message: msg !== '' ? msg : '입력을 제대로 하셔야합니다.' });
+	}
+};
 
 router
 	.route('/')
 	// 채널생성
-	.post(async (req, res) => {
-		const { user_id: id, channelTitle } = req.body;
-		const user_id = parseInt(id);
-		const [idCheck] = await conn.query(
-			`SELECT * FROM users WHERE id='${user_id}'`
-		);
-		const [dupleChannelTitle] = await conn.query(
-			`SELECT * FROM channels WHERE name='${channelTitle}'`
-		);
+	.post(
+		[
+			body('user_id')
+				.notEmpty()
+				.isInt()
+				.withMessage('아이디는 비어있지 않은 숫자로 입력하셔야 합니다.'),
+			body('channelTitle')
+				.notEmpty()
+				.isString()
+				.withMessage('채널명은 비어있지 않는 문자로 입력하셔야 합니다.'),
+			validation,
+		],
+		async (req, res, next) => {
+			const { user_id, channelTitle } = req.body;
 
-		if (idCheck.length > 0) {
-			await conn.query(
-				`INSERT INTO channels(name, user_id) VALUES ('${channelTitle}', '${user_id}')`
+			const [idCheck] = await conn.query(
+				`SELECT * FROM users WHERE id='${user_id}'`
 			);
-			res.status(201).json({
-				message: `${user_id}님 ${channelTitle}채널을 응원합니다.`,
-			});
-		} else if (idCheck.length === 0) {
-			return res.status(404).json({
-				message: `${id}는 찾을 수 없는 아이디입니다.`,
-			});
+			const [dupleChannelTitle] = await conn.query(
+				`SELECT * FROM channels WHERE name='${channelTitle}'`
+			);
+
+			if (idCheck.length > 0) {
+				if (dupleChannelTitle.length > 0) {
+					return res.status(409).json({
+						message: `${channelTitle} 이미 사용중인 채널명입니다.`,
+					});
+				}
+				await conn.query(
+					`INSERT INTO channels(name, user_id) VALUES ('${channelTitle}', '${user_id}')`
+				);
+				res.status(201).json({
+					message: `${user_id}님 ${channelTitle}채널을 응원합니다.`,
+				});
+			} else if (idCheck.length === 0) {
+				return res.status(404).json({
+					message: `${user_id}는 찾을 수 없는 아이디입니다.`,
+				});
+			}
 		}
-		if (dupleChannelTitle.length > 0) {
-			res.status(409).json({
-				message: `${channelTitle}은 이미 있는 채널명입니다.`,
-			});
-		}
-	})
+	)
 	// 채널전체조회
-	.get(async (req, res) => {
-		const { user_id: id } = req.body;
-		const user_id = parseInt(id);
-		const [idCheck] = await conn.query(
-			`SELECT * FROM channels WHERE user_id='${id}'`
-		);
+	.get(
+		[
+			body('user_id')
+				.notEmpty()
+				.isInt()
+				.withMessage('아이디는 비어있지 않은 숫자로 입력하셔야 합니다.'),
+			validation,
+		],
+		async (req, res, next) => {
+			const { user_id } = req.body;
+			const [userIdCheck] = await conn.query(
+				`SELECT * FROM users WHERE id='${user_id}'`
+			);
+			const [channelIdCheck] = await conn.query(
+				`SELECT * FROM channels WHERE user_id='${user_id}'`
+			);
 
-		if (!user_id) {
-			return res.status(404).json({
-				message: `${id}는 찾을 수 없는 아이디입니다.`,
-			});
+			if (userIdCheck.length === 0) {
+				return res.status(404).json({
+					message: `존재하지 않는 아이디입니다.`,
+				});
+			} else {
+				if (channelIdCheck.length > 0) {
+					return res.status(200).json(channelIdCheck.map((list) => list));
+				} else {
+					return res.status(404).json({
+						message: `${user_id}님의 조회할 채널이 없습니다.`,
+					});
+				}
+			}
 		}
-
-		if (idCheck.length > 0) {
-			res.status(200).json(idCheck.map((list) => list));
-		} else {
-			res.status(404).json({
-				message: `조회할 채널이 없습니다.`,
-			});
-		}
-	});
+	);
 
 router
 	.route('/:id')
 	// 채널개별수정
-	.put(async (req, res) => {
-		const { channelTitle, newTitle } = req.body;
-		const { id } = req.params;
-		const user_id = parseInt(id);
-		const [idCheck] = await conn.query(
-			`SELECT * FROM channels
+	.put(
+		[
+			body('channelTitle')
+				.notEmpty()
+				.isString()
+				.withMessage('채널명은 비어있지 않는 문자로 입력하셔야 합니다.'),
+			body('newTitle')
+				.notEmpty()
+				.isString()
+				.withMessage('채널명은 비어있지 않는 문자로 입력하셔야 합니다.'),
+			param('id')
+				.notEmpty()
+				.isInt()
+				.withMessage('아이디는 숫자로 입력하셔야 합니다.'),
+			validation,
+		],
+		async (req, res, next) => {
+			const { channelTitle, newTitle } = req.body;
+			const { id } = req.params;
+			const [userIdCheck] = await conn.query(
+				`SELECT * FROM users WHERE id='${id}'`
+			);
+
+			const [id_titleCheck] = await conn.query(
+				`SELECT * FROM channels
 			WHERE user_id = '${id}'
 			AND name = '${channelTitle}'`
-		);
-		const [dupleChannelTitle] = await conn.query(
-			`SELECT * FROM channels WHERE name='${newTitle}'`
-		);
-
-		if (idCheck.length === 0) {
-			return res.status(404).json({
-				message: `${user_id}는 찾을 수 없는 아이디입니다.`,
-			});
-		} else if (dupleChannelTitle.length > 0) {
-			return res.status(404).json({
-				message: `${newTitle}는 이미 있는 채널명입니다.`,
-			});
-		} else if (idCheck.length > 0) {
-			await conn.query(
-				`UPDATE channels SET name='${newTitle}' WHERE user_id='${id}'`
 			);
-			res.status(200).json({
-				message: `${user_id}님의 ${channelTitle}채널명이 ${newTitle}로 성공적으로 수정되었습니다.`,
-			});
+			const [dupleChannelTitle] = await conn.query(
+				`SELECT * FROM channels WHERE name='${newTitle}'`
+			);
+
+			if (userIdCheck.length === 0) {
+				return res.status(404).json({
+					message: `존재하지 않는 아이디입니다.`,
+				});
+			}
+			if (id_titleCheck.length === 0) {
+				return res.status(404).json({
+					message: `변경할 채널명이 존재하지 않습니다.`,
+				});
+			} else if (dupleChannelTitle.length > 0) {
+				return res.status(404).json({
+					message: `${newTitle} 이미 사용중인 채널명입니다.`,
+				});
+			} else {
+				await conn.query(
+					`UPDATE channels SET name='${newTitle}' WHERE name='${channelTitle}' AND user_id='${id}'`
+				);
+				return res.status(200).json({
+					message: `${id}님의 ${channelTitle}에서 ${newTitle}(으)로 성공적으로 수정되었습니다.`,
+				});
+			}
 		}
-	})
+	)
 	// 채널개별삭제
-	.delete(async (req, res) => {
-		const { channelTitle } = req.body;
-		const { id } = req.params;
-		const user_id = parseInt(id);
-		const [idCheck, fields] = await conn.query(
-			`SELECT * FROM channels WHERE user_id='${id}' AND name='${channelTitle}' `
-		);
+	.delete(
+		[
+			body('channelTitle')
+				.notEmpty()
+				.isString()
+				.withMessage('채널명은 비어있지 않는 문자로 입력하셔야 합니다.'),
+			param('id')
+				.notEmpty()
+				.isInt()
+				.withMessage('아이디는 숫자로 입력하셔야 합니다.'),
+			validation,
+		],
+		async (req, res, next) => {
+			const { channelTitle } = req.body;
+			const { id } = req.params;
+			const [userIdCheck] = await conn.query(
+				`SELECT * FROM users WHERE id='${id}'`
+			);
+			const [titleCheck] = await conn.query(
+				`SELECT * FROM channels WHERE user_id='${id}' AND name='${channelTitle}' `
+			);
 
-		if (idCheck.length > 0) {
-			await conn.query(`DELETE FROM channels WHERE user_id='${id}'`);
-			res.status(200).json({
-				message: `${user_id}님의 ${channelTitle}채널이 삭제되었습니다.`,
-			});
-		} else {
-			res.status(404).json({
-				message: `${user_id}님의 채널이 없습니다.`,
-			});
+			if (userIdCheck.length === 0) {
+				return res.status(404).json({
+					message: `존재하지 않는 아이디입니다.`,
+				});
+			}
+			if (titleCheck.length > 0) {
+				await conn.query(`DELETE FROM channels WHERE user_id='${id}'`);
+				return res.status(200).json({
+					message: `${id}님의 ${channelTitle}채널이 삭제되었습니다.`,
+				});
+			} else {
+				return res.status(404).json({
+					message: `${id}님의 채널이 없습니다.`,
+				});
+			}
 		}
-	})
+	)
 	// 채널개별조회
-	.get(async (req, res) => {
-		const { id } = req.params;
-		const user_id = parseInt(id);
-		const [idCheck, fields] = await conn.query(
-			`SELECT * FROM channels WHERE user_id='${id}'`
-		);
+	.get(
+		[
+			param('id')
+				.notEmpty()
+				.isInt()
+				.withMessage('아이디는 숫자로 입력하셔야 합니다.'),
+			validation,
+		],
+		async (req, res, next) => {
+			const { id } = req.params;
+			const [userIdCheck] = await conn.query(
+				`SELECT * FROM users WHERE id='${id}'`
+			);
+			const [channelIdCheck] = await conn.query(
+				`SELECT * FROM channels WHERE user_id='${id}'`
+			);
 
-		if (idCheck.length > 0) {
-			res.status(200).json(idCheck);
-		} else {
-			res.status(400).json({
-				message: `${user_id}는 존재하지 않는 아이디입니다.`,
-			});
+			if (userIdCheck.length === 0) {
+				return res.status(404).json({
+					message: `존재하지 않는 아이디입니다.`,
+				});
+			}
+			if (channelIdCheck.length > 0) {
+				return res.status(200).json(channelIdCheck);
+			} else {
+				return res.status(400).json({
+					message: `${id}님의 채널이 존재하지않습니다.`,
+				});
+			}
 		}
-	});
+	);
 
 export default router;
--- routes/users.js
+++ routes/users.js
@@ -1,86 +1,162 @@
 import { Router } from 'express';
 const router = Router();
 import conn from '../mariadb.js';
+import { body, param, query, validationResult } from 'express-validator';
+
+// JWT 모듈
+import jwt from 'jsonwebtoken';
+const { sign, verify } = jwt;
+
+// dotenv 모듈
+import { config } from 'dotenv';
+config();
+
+const validation = (req, res, next) => {
+	const err = validationResult(req);
+	if (err.isEmpty()) {
+		return next();
+	} else {
+		console.log(err.array());
+
+		const msg = err
+			.array()
+			.filter((n) => n.msg !== 'Invalid value')
+			.map((n) => n.msg)
+			.join('');
+
+		return res
+			.status(404)
+			.json({ message: msg !== '' ? msg : '입력을 제대로 하셔야합니다.' });
+	}
+};
 
 // 로그인
-router.post('/login', async (req, res) => {
-	const { email, password } = req.body;
-
-	const [results, fields] = await conn.query(
-		`SELECT * FROM users WHERE email='${email}' AND password=${password}`
-	);
-
-	if (results.length > 0) {
-		res.status(200).json({
-			message: `${email}님 환영합니다.`,
-		});
-	} else {
-		res.status(400).json({
-			message: `아이디나 비밀번호를 다시 확인해주세요.`,
-		});
-	}
-});
-
-// 회원가입
-router.post('/join', async (req, res) => {
-	const { email, name, password, contact } = req.body;
-
-	const [results, fields] = await conn.query(
-		`SELECT * FROM users WHERE email='${email}'`
-	);
-	if (results.length > 0) {
-		res.status(404).json({
-			message: `이미 있는 계정입니다.`,
-		});
-	} else {
-		await conn.query(
-			`INSERT INTO users(email, name, password, contact)
-			VALUES( '${email}', '${name}', '${password}', '${contact}' )`
-		);
-		res.status(200).json({
-			message: `${email}님 환영합니다.`,
-		});
-	}
-});
-
-router
-	.route('/users')
-	// 회원개별조회
-	.get(async (req, res) => {
-		const { email } = req.body;
-
-		const [results, fields] = await conn.query(
-			`SELECT * FROM users WHERE email='${email}'`
-		);
-
-		if (results.length > 0) {
-			res.status(200).json(results);
-		} else {
-			res.status(404).json({
-				message: `존재하지 않는 계정입니다.`,
-			});
-		}
-	})
-	// 회원개별탈퇴
-	.delete(async (req, res) => {
+router.post(
+	'/login',
+	[body('email').notEmpty().isEmail(), body('password').notEmpty(), validation],
+	async (req, res, next) => {
 		const { email, password } = req.body;
 
-		const [results, fields] = await conn.query(
+		const [results] = await conn.query(
 			`SELECT * FROM users WHERE email='${email}' AND password=${password}`
 		);
 
 		if (results.length > 0) {
-			await conn.query(
-				`DELETE FROM users WHERE email ='${email}' AND password = ${password}`
+			const [user_id] = await conn.query(
+				`SELECT id FROM users WHERE email='${email}'`
 			);
+			const [id] = user_id;
+
+			const token = sign(
+				{
+					email,
+					user_id: id.id,
+				},
+				process.env.PRIVATE_KEY,
+				{
+					expiresIn: '5m',
+					issuer: 'YouD',
+				}
+			);
+
+			res.cookie('token', token, { httpOnly: true });
+
 			res.status(200).json({
-				message: `${email}님의 계정이 탈퇴되었습니다.`,
+				message: `${email}님 환영합니다.`,
 			});
 		} else {
-			res.status(404).json({
-				message: `존재하지 않는 계정입니다.`,
+			res.status(403).json({
+				message: `아이디나 비밀번호를 다시 확인해주세요.`,
 			});
 		}
-	});
+	}
+);
+
+// 회원가입
+router.post(
+	'/join',
+	[
+		body('email').notEmpty().isEmail(),
+		body('name').notEmpty(),
+		body('password').notEmpty(),
+		body('contact').notEmpty(),
+		validation,
+	],
+	async (req, res, next) => {
+		const { email, name, password, contact } = req.body;
+
+		const [results] = await conn.query(
+			`SELECT * FROM users WHERE email='${email}'`
+		);
+		if (results.length > 0) {
+			res.status(404).json({
+				message: `이미 있는 계정입니다.`,
+			});
+		} else {
+			await conn.query(
+				`INSERT INTO users(email, name, password, contact)
+			VALUES( '${email}', '${name}', '${password}', '${contact}' )`
+			);
+			res.status(200).json({
+				message: `${email}님 환영합니다.`,
+			});
+		}
+	}
+);
+
+router
+	.route('/users')
+	// 회원개별조회
+	.get(
+		[body('email').notEmpty().isEmail(), validation],
+		async (req, res, next) => {
+			const { email } = req.body;
+
+			const [results] = await conn.query(
+				`SELECT * FROM users WHERE email='${email}'`
+			);
+
+			if (results.length > 0) {
+				res.status(200).json(results);
+			} else {
+				res.status(404).json({
+					message: `존재하지 않는 계정입니다.`,
+				});
+			}
+		}
+	)
+	// 회원개별탈퇴
+	.delete(
+		[
+			body('email').notEmpty().isEmail(),
+			body('password').notEmpty(),
+			validation,
+		],
+		async (req, res, next) => {
+			const { email, password } = req.body;
+
+			const [results] = await conn.query(
+				`SELECT * FROM users WHERE email='${email}' AND password=${password}`
+			);
+			const [user_id] = await conn.query(`
+				SELECT id FROM users WHERE email='${email}'`);
+			const [id] = user_id;
+
+			if (results.length > 0) {
+				await conn.query(`DELETE FROM channels WHERE user_id ='${id.id}'`);
+				await conn.query(
+					`DELETE FROM users WHERE email ='${email}' AND password = '${password}'`
+				);
+
+				res.status(200).json({
+					message: `${email}님의 계정이 탈퇴되었습니다.`,
+				});
+			} else {
+				res.status(404).json({
+					message: `존재하지 않는 계정입니다.`,
+				});
+			}
+		}
+	);
 
 export default router;
